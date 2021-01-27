9ce18aa7e34b756637562da717f030f237ca2d1e
sfynsss <sofyansss378@gmail.com>
2021-01-27 23:43:53 +0700

    Update RegisterController.php

--- app/Http/Controllers/Api/Auth/RegisterController.php
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -75,7 +75,7 @@ class RegisterController extends Controller
 			]);
 
 			if ($save) {
-				$register = User::select('users.*', 'customer.JNS_KELAMIN')->where('id', '=', $user)->join('customer', 'customer.id', '=', 'users.id')->first();
+				$register = User::select('users.*', 'customer.JNS_KELAMIN')->where('users.id', '=', $user)->join('customer', 'customer.id', '=', 'users.id')->first();
 
 				$name = $register['name'];
 				$token = $register['activation_token'];
