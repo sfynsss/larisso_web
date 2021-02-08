b45cd92ec39ff248f6583774b720fb8252efda51
sfynsss <sofyansss378@gmail.com>
2021-02-08 15:11:39 +0700

    commit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -16,7 +16,7 @@ class Controller extends BaseController
     	// Set your Merchant Server Key
 		\Midtrans\Config::$serverKey = 'Mid-server-xTaMzZDeY2QEujZxMmpTXxIW';
 // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
-		\Midtrans\Config::$isProduction = false;
+		\Midtrans\Config::$isProduction = true;
 // Set sanitization on (default)
 		\Midtrans\Config::$isSanitized = true;
 // Set 3DS transaction for credit card to true
