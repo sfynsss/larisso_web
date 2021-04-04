1f0b686a152696197cfd467e4747b1faa7d26f97
Biyonda Kurniawan <biyondakurniawan@gmail.com>
2021-04-05 02:18:41 +0700

    Update PembayaranController.php

--- app/Http/Controllers/PembayaranController.php
+++ app/Http/Controllers/PembayaranController.php
@@ -21,11 +21,11 @@ class PembayaranController extends Controller
 	{
 		$this->request = $request;
         // Set your Merchant Server Key
-		//\Midtrans\Config::$serverKey = 'Mid-server-xTaMzZDeY2QEujZxMmpTXxIW';
-		\Midtrans\Config::$serverKey = 'SB-Mid-server-fQhLQ_KX1fnc33JY51LS8Il0';
+		\Midtrans\Config::$serverKey = 'Mid-server-xTaMzZDeY2QEujZxMmpTXxIW';
+		//\Midtrans\Config::$serverKey = 'SB-Mid-server-fQhLQ_KX1fnc33JY51LS8Il0';
         // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
-		\Midtrans\Config::$isProduction = false;
-		//\Midtrans\Config::$isProduction = true;
+		//\Midtrans\Config::$isProduction = false;
+		\Midtrans\Config::$isProduction = true;
         // Set sanitization on (default)
 		\Midtrans\Config::$isSanitized = true;
         // Set 3DS transaction for credit card to true
